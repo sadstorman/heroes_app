9b87809c1ac9d277a9555e3fa8450c585a5de52f
Juan Lia <liajuancruz@gmail.com>
2021-09-30 11:30:08 -0300

    otra prueba fotos

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -4,7 +4,7 @@ import { HeroList } from '../heroes/HeroList'
 export const DcScreen = () => {
     return (
         <div>
-            <img alt="marvel" src="/assets/dc.jpg" className="img img-fluid rounded mx-auto d-block w-25 animate__animated animate__fadeIn"/>
+            <img alt="marvel" src="assets/dc.jpg" className="img img-fluid rounded mx-auto d-block w-25 animate__animated animate__fadeIn"/>
             
             <div>
                 <HeroList publisher={"DC Comics"} />
